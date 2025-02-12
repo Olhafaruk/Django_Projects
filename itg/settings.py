70768c85c3b482d9072e33b5c3c3e271b417b43d
Olha Faruk <farukolga2017@gmail.com>
2025-02-12 19:09:27 +0100

    Lesson 9

--- itg/settings.py
+++ itg/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = 'itg.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'itg-230924',
+           'USER': 'postgres',
+           'PASSWORD': 'Admin',
+           'HOST': 'localhost',
+           'PORT': '5432',
+       }
+   }
 
 
 # Password validation
