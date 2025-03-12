5563f46f6f8f73e25d6727a5544a60987c599835
Olha Faruk <farukolga2017@gmail.com>
2025-03-12 21:11:59 +0100

    добавили возможность доÐ+image

--- itg/urls.py
+++ itg/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('news/', include('news.urls', namespace='news')),
     path('news/', include('news.urls')),
 ]
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
